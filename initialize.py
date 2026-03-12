a066d60c603a937e73cf01e6abd25e69935db572
keng1202 <okk15181@gmail.com>
2026-03-12 23:57:18 +0800

    修正data size 和input size混和問題

--- initialize.py
+++ initialize.py
@@ -2,6 +2,7 @@ import random
 
 input_size = 500
 par_range = 1000
+data_size = 1000
 
 model = [random.randint(-par_range, par_range) for _ in range(input_size)]
 
@@ -11,7 +12,7 @@ for i in range(input_size):
 fout.close()
 
 fout = open("training.csv", "w")
-for i in range(input_size):
+for i in range(data_size):
     par = [random.uniform(-par_range, par_range) for _ in range(input_size)]
     for j in range(input_size):
         fout.write(str(par[j]))
@@ -24,7 +25,7 @@ fout.close()
 
 fout = open("testing.csv", "w")
 fout2 = open("ans.csv", "w")
-for i in range(input_size):
+for i in range(data_size):
     par = [random.uniform(-par_range, par_range) for _ in range(input_size)]
     for j in range(input_size):
         fout.write(str(par[j]))
